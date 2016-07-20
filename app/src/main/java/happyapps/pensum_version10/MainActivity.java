33d30b49965bb31aa8fb0b566f0f422990a5dfce
Longynus <hacpin@hotmail.com>
2016-07-20 14:38:57 -0500

    Version 1.0 probada y funcionando

---
+++ app/src/main/java/happyapps/pensum_version10/MainActivity.java
@@ -0,0 +1,24 @@
+package happyapps.pensum_version10;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //iniciamos el banner cuando se crea la activity
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+
+}
