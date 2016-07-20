803a5f97599973d2da78fef028a43aa38a628797
Longynus <hacpin@hotmail.com>
2016-07-20 16:49:14 -0500

    Version 1.1 Arreglados los paddings de los botones y cada boton va a su activity determinado

--- app/src/main/java/happyapps/pensum_version10/MainActivity.java
+++ app/src/main/java/happyapps/pensum_version10/MainActivity.java
@@ -1,7 +1,9 @@
 package happyapps.pensum_version10;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -14,11 +16,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        //iniciamos el banner cuando se crea la activity
+        //iniciamos el banner ad cuando se crea la activity
         AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
     }
 
+
+    //metodo para lanzar el la pantalla de sedes
+    public void lanzarSedes(View view) {
+        Intent i = new Intent(this, sedes.class );
+        startActivity(i);
+    }
+
+    //metodo para lanzar el la pantalla de notificaciones
+    public void lanzarNotificaciones(View view) {
+        Intent i = new Intent(this, notificaciones.class );
+        startActivity(i);
+    }
+
+    //metodo para lanzar el la pantalla de sia
+    public void lanzarSia(View view) {
+        Intent i = new Intent(this, sia.class );
+        startActivity(i);
+    }
+
+    //metodo para lanzar el la pantalla de programas
+    public void lanzarProgramas(View view) {
+        Intent i = new Intent(this, programas.class );
+        startActivity(i);
+    }
 
 }
